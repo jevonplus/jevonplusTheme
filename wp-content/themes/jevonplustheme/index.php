17c535137d2684ccb3640ce00e2542bab389167e
jevonplus <jevonplus@163.com>
2020-11-30 18:19:43 +0800

    modify index and csss bg

--- wp-content/themes/jevonplustheme/index.php
+++ wp-content/themes/jevonplustheme/index.php
@@ -4,28 +4,12 @@
 <body>
 <h1><?php bloginfo( 'name' ); ?></h1>
 <h2><?php bloginfo( 'description' ); ?></h2>
- 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
- 
-<h3><?php the_title(); ?></h3>
- 
-<?php the_content(); ?>
-<?php wp_link_pages(); ?>
-<?php edit_post_link(); ?>
- 
-<?php endwhile; ?>
- 
-<?php
-if ( get_next_posts_link() ) {
-next_posts_link();
-}
-?>
-<?php
-if ( get_previous_posts_link() ) {
-previous_posts_link();
-}
-?>
-<?php endif; ?>
+     <ul id="starlist">
+      <li><a href="index.php">首页</a></li>
+      <li><a href="share.php">相册</a></li>
+      <li><a href="list.php">博文</a></li>
+      <li><a href="about.php">关于</a></li>
+     </ul>
 <?php get_footer(); ?>
 </body>
 </html>
